e8ad4f8cf32666a48bdd11b9ce34fc01dd7f1fbd
mgsainyinyitun <mgsainyinyitun.tumdy@gmail.com>
2023-12-12 13:30:53 +0630

    npm run build

--- src/components/tkblist/TkbItem.jsx
+++ src/components/tkblist/TkbItem.jsx
@@ -2,7 +2,6 @@ function TkbItem({ index, id, occureDate, group, server, serverity, fase, messag
 
     function convertISOToFormattedString(isoString) {
         const date = new Date(isoString);
-
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
